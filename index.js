cb2378881ba3948ef6777d3dbfd3a72a4bb65c04
zikrullahaliwainda <zikrullahaliwainda@gmail.com>
2020-03-22 01:10:14 +0700

    add comment explanation

--- index.js
+++ index.js
@@ -1,139 +1,142 @@
 //Create By Zik 13-03-2020
-
+//inisialisasi standar node js
 const http = require('http');
+//inisialisasi middleware
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+//inisialisasi framework nodejs
 const express = require('express');
 const app = express();
+//menghubungkan nodejs ke http
 const server = http.Server(app);
+//inisialisasi port
 const port = 8080;
+//inisialisasi data dari Nominations.json
 var data = require("./data/Nominations.json")
+    //inisialisasi library JSONRPC
 const jayson = require("jayson");
+//inisialisasi socket.io
 const io = require("socket.io")(server);
+//inisialisasi current user
 var clients = [];
+//memasang middleware
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req,res,next) => {
+//menentukan header dan batasan pada endpoint
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
-	next();
-} );
-
-// var vote_amount = [];
-// for(var i = 0 ; i < data.data.length;i++){
-//     var temp = [];
-//     for (var j = 0 ; j<data.data[i].nominations.length;j++) {
-//         temp.push(0);
-//     }
-//     vote_amount.push(temp);
-// }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+    next();
+});
+//variable yang nanti digunakan client melalui JSONRPC
 const vote = {
-    vote: function(args, callback) {
-        // callback(null, data);
-        var socketIndex = getIndexMacAddress(args[0].macaddress);
-        console.log(data.data[args[0].idnominations-1]
-            .nominations[args[0].idnominator-1]);
-        if(socketIndex != undefined && !clients[socketIndex].voteBool){
-            
-            data.data[args[0].idnominations-1]
-            .nominations[args[0].idnominator-1].vote = 
-            data.data[args[0].idnominations-1]
-            .nominations[args[0].idnominator-1].vote + 1;
-            
-            getPercentage(args[0].idnominations-1,); 
-            
-            // vote_amount[args[0].idnominations][args[0].idnominator] += 1; 
-            callback(null,{
-                status : 1,
-                message : "Succesfully Vote",
-                params: args == undefined ? 1 : args[0]
-            });
-            io.emit('update',data);
-            console.log("Nominations Voted");
-        }else
-            callback(null,{
-                status : 0,
-                message : "Failed Vote",
+        vote: function(args, callback) {
+            //socket id user diambil dan memulai proses voting
+            var socketIndex = getIndexMacAddress(args[0].macaddress);
+            console.log(data.data[args[0].idnominations - 1]
+                .nominations[args[0].idnominator - 1]);
+            if (socketIndex != undefined && !clients[socketIndex].voteBool) {
+
+                data.data[args[0].idnominations - 1]
+                    .nominations[args[0].idnominator - 1].vote =
+                    data.data[args[0].idnominations - 1]
+                    .nominations[args[0].idnominator - 1].vote + 1;
+                //menghitung ulang persentasi dari semua nominasi
+                getPercentage(args[0].idnominations - 1, );
+                //mengirim data terbaru setalah voting
+                callback(null, {
+                    status: 1,
+                    message: "Succesfully Vote",
+                    params: args == undefined ? 1 : args[0]
+                });
+                io.emit('update', data);
+                console.log("Nominations Voted");
+            } else
+            //mengirim pesan jika tidak berhasil vote
+                callback(null, {
+                status: 0,
+                message: "Failed Vote",
                 params: args == undefined ? 1 : args[0]
             })
-        // console.log(data.data[args[0].idnominations]
-        //     .nominations[args[0].idnominator].vote +" = "+
-        //     vote_amount[args[0].idnominations][args[0].idnominator])
+        }
     }
-}
-
-// app.get('/',function(req,res){
-//     res.send('hello world');
-//     console.log("Client Connected");
-// })
-
-io.sockets.on('connection',(socket)=>{
-    console.log("New User Connected");
-
-    socket.on("SaveCustomId",function(body){
-        var socketIndex = getIndexMacAddress(JSON.parse(body).macAddress); 
-        console.log(socketIndex);
-        if(socketIndex == undefined){
-            var clientInfo = new Object();
-            clientInfo.macAddress = JSON.parse(body).macAddress;
-            clientInfo.socketId = socket.id;
-            clientInfo.voteBool = false;
-            clients.push(clientInfo);
-        }else{
-            clients[socketIndex].socketId = socket.id;
-        }
-        socket.emit('message',data);
+    //pemasangan socket dan handler jika ada user yang connect
+io.sockets.on('connection', (socket) => {
+        console.log("New User Connected");
+        //menerima request dari client jika dia meminta saveCustomId
+        socket.on("SaveCustomId", function(body) {
+                //menyimpan data data user ke variable client
+                var socketIndex = getIndexMacAddress(JSON.parse(body).macAddress);
+                console.log(socketIndex);
+                if (socketIndex == undefined) {
+                    var clientInfo = new Object();
+                    clientInfo.macAddress = JSON.parse(body).macAddress;
+                    clientInfo.socketId = socket.id;
+                    clientInfo.voteBool = false;
+                    clients.push(clientInfo);
+                } else {
+                    clients[socketIndex].socketId = socket.id;
+                }
+                //mengirim pesan berhasil
+                socket.emit('message', data);
+            })
+            //menerima request getListNomination dari client
+        socket.on("getListNominations", function(data) {
+                //mengambil socket id dan mengirimkan data terbaru 
+                var socketId = getSocketId(data.macAddress);
+                if (socketId != undefined) {
+                    socket.to(socketId).emit('message', data);
+                }
+            })
+            //handler jika ada client yang disconnect 
+        socket.on('disconnect', function(data) {
+            //menghapus data client di variable client
+            var socketIndex = getIndexMacAddress(data.macAddress);
+            if (socketIndex != undefined) {
+                clients[socketIndex].socketId == undefined;
+            }
+            console.log("a user Disconnected");
+        })
     })
-    socket.on("getListNominations",function(data){
-        var socketId = getSocketId(data.macAddress);
-        if(socketId != undefined){
-            socket.to(socketId).emit('message',data);
-        }
-    })
+    //memasang const vote ke endpoint dengan method post
+app.post('/', jayson.server(vote).middleware())
 
-    socket.on('disconnect',function(data){
-        var socketIndex = getIndexMacAddress(data.macAddress);
-        if(socketIndex != undefined){
-            clients[socketIndex].socketId == undefined;
-        }
-        console.log("a user Disconnected");
-    })
-})
-app.post('/',jayson.server(vote).middleware())
-
-server.listen(port,(err)=>{
-    if(err){
+//server mulai mendengarkan request
+server.listen(port, (err) => {
+    if (err) {
         throw err;
     }
     console.log("NodePoints Worked Now !!");
 })
 
-function getSocketId(macAddress){
-    for(x in clients){
-        if(x.macAddress == macAddress)
+//method untuk mengambil socket id berdasarkan mac address
+function getSocketId(macAddress) {
+    for (x in clients) {
+        if (x.macAddress == macAddress)
             return x.socketId;
         else return undefined;
     }
 }
 
-function getIndexMacAddress(macAddress){
-    for(var i = 0; i < clients.length;i++){
-        if(clients[i].macAddress == macAddress)
+//method untuk mengambil index berdasarkan mac address
+function getIndexMacAddress(macAddress) {
+    for (var i = 0; i < clients.length; i++) {
+        if (clients[i].macAddress == macAddress)
             return i;
         else return undefined;
     }
 }
-function getPercentage(idnominations){
+//method untuk menghitung ulang persentasi setiap nominasi
+function getPercentage(idnominations) {
     var counter = 0;
-    for(var i = 0; i < data.data[idnominations].nominations.length;i++){
+    for (var i = 0; i < data.data[idnominations].nominations.length; i++) {
         counter += data.data[idnominations].nominations[i].vote
     }
     counter = counter == 0 ? 10 : counter;
-    for(var j = 0 ; j < data.data[idnominations].nominations.length;j++){
-        data.data[idnominations].nominations[j].percentage = 
-        Math.round((data.data[idnominations].nominations[j].vote / counter)*100) +"%"
+    for (var j = 0; j < data.data[idnominations].nominations.length; j++) {
+        data.data[idnominations].nominations[j].percentage =
+            Math.round((data.data[idnominations].nominations[j].vote / counter) * 100) + "%"
     }
 }
